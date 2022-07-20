a2855d79c4d49f44ddbab1e8bd2176fd77bee91a
super <kelten3000@gmail.com>
2022-07-20 09:53:34 -0400

    Configure postgres db using docker

--- keltenservices/customer/src/main/java/com/keltenfalez/customer/CustomerService.java
+++ keltenservices/customer/src/main/java/com/keltenfalez/customer/CustomerService.java
@@ -3,12 +3,14 @@ package com.keltenfalez.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+
+        customerRepository.save(customer);
     }
 }
