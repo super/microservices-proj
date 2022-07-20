77a4a04f16a67b957de6747def5055c3ef083d7e
super <kelten3000@gmail.com>
2022-07-20 07:41:09 -0400

    fixed parents pom

--- keltenservices/customer/src/main/java/com/keltenfalez/customer/CustomerApplication.java
+++ keltenservices/customer/src/main/java/com/keltenfalez/customer/CustomerApplication.java
@@ -1,2 +1,11 @@
-package com.keltenfalez.customer;public class CustomerApplication {
+package com.keltenfalez.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
 }
