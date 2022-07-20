4a9e5df3545bcb2afd90991fd723ee22a911d8a3
super <kelten3000@gmail.com>
2022-07-20 08:12:25 -0400

    Basic Customer microservice

---
+++ keltenservices/customer/src/main/java/com/keltenfalez/customer/CustomerService.java
@@ -0,0 +1,14 @@
+package com.keltenfalez.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+    }
+}
